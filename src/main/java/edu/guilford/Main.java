d863d3da666894e8d275f3b5b1c1d30f157ee168
Pranavi Josyula <pjosyula@guilford.edu>
2024-09-19 14:01:55 -0400

    hey dude

--- src/main/java/edu/guilford/Main.java
+++ src/main/java/edu/guilford/Main.java
@@ -1,11 +1,20 @@
 package edu.guilford;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+     Random rand = new Random();
     public static void main(String[] args) {
 
         // default for loop, prints loop variable as it counts up
+        Scanner scan = new Scanner(System.in);
+        System.out.println("You are going to be rolling ten dice. How many sides should the dice have?");
+        int sides = scan.nextInt();
+        int sum = 0;
         for (int i = 0; i < 10; i++) {
-            System.out.println("loop variable i = " + i);
+            int diceRoll = 0;
+            sum += diceRoll;
+            System.out.println("Sum of " + i + "d" + sides + " = " + sum);
         }
     }
 }
